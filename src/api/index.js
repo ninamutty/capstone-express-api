b8bce1010684a280159fe0d2756106480d6ba0fe
Nina Mutty <ninamutty@gmail.com>
2017-01-10 15:02:55 -0800

    added cors for cross-origin-requests

--- src/api/index.js
+++ src/api/index.js
@@ -5,40 +5,14 @@ var express = require('express');
 var User = require('../models/user.js');
 var router = express.Router();
 const mongoose = require('mongoose');
+// var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+// var config = require('../../config'); // get our config file
+
 
 
 // require('./subscriptions_path.js');
 // require('./users_path.js');
 
-//
-// router.post('/authenticate', function(req, res) {
-//   // find the user
-//   User.findOne({
-//     email: req.body.email
-//   }, function(err, user) {
-//     if (err) throw err;
-//     if (!user) {
-//       res.json({ success: false, message: 'Authentication failed. User not found.' });
-//     } else if (user) {
-//       // check if password matches
-//       if (user.password != req.body.password) {
-//         res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-//     } else {
-//       // if user is found and password is right
-//         // create a token
-//         var token = jwt.sign(user, app.get('superSecret'), {
-//           expiresInMinutes: 1440 // expires in 24 hours
-//         });
-//         // return the information including token as JSON
-//         res.json({
-//           success: true,
-//           message: 'Enjoy your token!',
-//           token: token
-//         });
-//       }
-//     }
-//   });
-// });
 
 
 // route middleware to verify a token
--- src/app.js
+++ src/app.js
@@ -8,14 +8,11 @@ var mongoose = require('mongoose');
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('../config'); // get our config file
 var User   = require('./models/user'); // get our mongoose model
-
+var cors = require('cors');
 
 var app = express();
 
-// require('./database.js');
-// require('./seeds/subscriptions.js');
 require('./seeds/users.js');
-// require('./api/index.js');
 
 
 var port = process.env.PORT || 3000; // used to create, sign, and verify tokens
@@ -36,7 +33,7 @@ router.get('/', function(req, res) {
 });
 
 app.use(morgan('dev'));
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -44,35 +41,5 @@ app.listen(port);
 console.log('Magic happens at http://localhost:' + port);
 
 
-// route middleware to verify a token
-// router.use(function(req, res, next) {
-//   // check header or url parameters or post parameters for token
-//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
-//   // decode token
-//   if (token) {
-//     // verifies secret and checks exp
-//     jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-//       if (err) {
-//         return res.json({ success: false, message: 'Failed to authenticate token.' });
-//       } else {
-//         // if everything is good, save to request for use in other routes
-//         req.decoded = decoded;
-//         next();
-//       }
-//     });
-//   } else {
-//     // if there is no token
-//     // return an error
-//     return res.status(403).send({
-//         success: false,
-//         message: 'No token provided.'
-//     });
-//   }
-// });
-
-
-// route to show a random message (GET http://localhost:8080/api/)
-// route to return all users (GET http://localhost:8080/api/users)
-// apply the routes to our application with the prefix /api
 
 app.use('/api', router);  //first parameter is name space and second route is the router - routes will automatically be added
