1edb2aad99c405b07375630ba10af2bdd0cef561
Nina Mutty <ninamutty@gmail.com>
2017-01-25 14:09:05 -0800

    removed localhost from cors

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ninamutty.github.io/subscription-tracker-deployment, http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://ninamutty.github.io/subscription-tracker-deployment');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
