63372aa2d7406b0884d1c9a8e4a97103cddc391a
Nina Mutty <ninamutty@gmail.com>
2017-01-25 22:07:55 -0800

    changed domain for cors

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://ninamutty.github.io/subscription-tracker-deployment');
+    res.setHeader('Access-Control-Allow-Origin', 'https://subscription-tracker-2.herokuapp.com/');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
