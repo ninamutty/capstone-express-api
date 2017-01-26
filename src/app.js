b59fb61e342e82853cc0276d29a638fe97b9e1cb
Nina Mutty <ninamutty@gmail.com>
2017-01-26 14:05:33 -0800

    changed cors to custom domain

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(cors());
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://subscription-tracker-2.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://www.subtracked.co');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
